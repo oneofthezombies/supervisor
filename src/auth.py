b656cebacca8c170aab319c7d7615d34b80e041d
oneofthezombies <hunhoekim@gmail.com>
2024-06-20 15:13:22 +0900

    wip: token login

--- src/auth.py
+++ src/auth.py
@@ -1,22 +1,23 @@
+from typing_extensions import Annotated
 from fastapi import Depends, HTTPException, status
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.common import RoleEnum
 from src import database, models
-from services.user import UserService
+from services.user import UserService, UserServiceDep
 
 
 security = HTTPBasic()
 
+BasicDep = Annotated[HTTPBasicCredentials, Depends(security)]
+
 
 async def get_current_user(
-    credentials: HTTPBasicCredentials = Depends(security),
-    user_service: UserService = Depends(UserService.get),
+    basic: BasicDep,
+    user_service: UserServiceDep,
 ):
-    user = await user_service.authenticate_user(
-        credentials.username, credentials.password
-    )
+    user = await user_service.authenticate_user(basic.username, basic.password)
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,

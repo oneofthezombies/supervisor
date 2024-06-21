65a6d84c5ad34cd8e003bd37bbb3534ee8aeff45
oneofthezombies <hunhoekim@gmail.com>
2024-06-21 13:00:43 +0900

    test: add test

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,8 +5,21 @@ from app.main import app
 from app import database
 
 
+@pytest.fixture
+def anyio_backend() -> str:
+    return "asyncio"
+
+
 @pytest.fixture(scope="module", autouse=True)
 async def setup_db():
-    database.Base.metadata.create_all(database.engine)
+    async with database.engine.begin() as conn:
+        await conn.run_sync(database.Base.metadata.create_all)
     yield
-    database.Base.metadata.drop_all(database.engine)
+    async with database.engine.begin() as conn:
+        await conn.run_sync(database.Base.metadata.drop_all)
+
+
+@pytest.fixture
+async def client():
+    async with AsyncClient(app=app, base_url="http://test") as ac:
+        yield ac
